b93d3007073248dca06a83509ddd716f9280491c
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-16 13:42:55 +0700

    merubah file app dan menambahkan package

--- App.js
+++ App.js
@@ -1,52 +1,68 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+    const express = require('express');
+    const expressLayouts = require('express-ejs-layouts');
 
-const app = express();
-const port = 3000;
+    const app = express();
+    const port = 3000;
 // ============================================= setup ejs ===========================================
-app.set('view engine','ejs');
-app.use(expressLayouts);//third party middleware
-app.use(express.static('public'));//built in middleware
-app.use(express.urlencoded({extended:true}))
-
-
-
-
-
-
-
-
+    app.set('view engine','ejs');
 //================================================= Route =====================================
-app.get('/',(req,res) => {
+    app.get('/',(req,res) => {
     
     // res.json(
     //     {user:'tobi',email:'@GMAIL.COM'},
         
     // )
-    res.sendFile('./views/index.html',{root :__dirname})
-});
+    // res.sendFile('./views/index.html',{root :__dirname})
+    const mahasiswa = [
+        {
+            nama:'Dede syarifudin',
+            email :'dede@gmail.com'
+        },
+        {
+            nama:' syarifudin',
+            email :'syarifudin@gmail.com'
+        },
+        {
+            nama:'alhadid07',
+            email :'alhadid07@gmail.com'
+        }, 
+        {
+            nama:'ahmadbedoel',
+            email :'ahmad@gmail.com'
+        }
+        
+    ]
+    res.render('index',{
+        nama:'Dedesyarifudin' ,
+        title:'halaman home',
+        mahasiswa,
+    })
+    
+    });
 
-app.get('/about',(req,res) => {
+    app.get('/about',(req,res) => {
     // res.send('halaman about ')
-    res.sendFile('./views/about.html',{root :__dirname})
-});
+    // res.sendFile('./views/about.html',{root :__dirname})
+    res.render('about')
+    });
 
-app.get('/contact',(req,res) => {
+    app.get('/contact',(req,res) => {
     // res.send('halaman contack')
-    res.sendFile('./views/contact.html',{root :__dirname})
-});
+    // res.sendFile('./views/contact.html',{root :__dirname})
+    res.render('contact')
+    });
 
 
-app.get('/product/:id',(req,res) => {
+    app.get('/product/:id',(req,res) => {
     res.send('producy id :' + req.params.id)
-})
+    })
 
-app.use('/',(req,res) => {
+    app.use('/',(req,res) => {
     res.status(404)
     res.send('404')
-})
+    })
 
-app.listen(port,(req,res) => {
-   console.log(`mongo contact app || listenig at http://localhost:${port}`);
-   
-})
+    app.listen(port,(req,res) => {
+    console.log(`mongo contact app || listenig at http://localhost:${port}`);
+    
+    })
